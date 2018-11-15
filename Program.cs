beedc9989823f87b304e29b8ae7fd29a2d959359
malar87 <44968450+malar87@users.noreply.github.com>
2018-11-15 12:58:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i+1);
+            }
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            var list1 = list.OrderByDescending(item=> item);
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+   
+}
